7005ffe1ab6067d79df003279eda97043cf2eb9a
alan <alanspina@gmail.com>
2020-05-03 18:50:39 -0300

    Cargue las imagenes e hice el card view

--- app/src/main/java/Producto.java
+++ app/src/main/java/Producto.java
@@ -1,2 +1,35 @@
 public class Producto {
+    private String nombre;
+    private Integer precio;
+    private Integer imagen;
+
+    public Producto(String nombre, Integer precio, Integer imagen) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public Integer getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(Integer imagen) {
+        this.imagen = imagen;
+    }
 }
