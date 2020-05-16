4916e492eee6c30ed17aa99cf04f4b6b52b3f465
alan <alanspina@gmail.com>
2020-05-16 18:19:11 -0300

    ERROR AL setNavigationItemSelectedListener

--- app/src/main/java/com/example/mercadoesclavospinaalan/MainActivity.java
+++ app/src/main/java/com/example/mercadoesclavospinaalan/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.mercadoesclavospinaalan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById();
+
+       // navigationView.setNavigationItemSelectedListener(); -> ERROR NO ME APARECE LA OPCION EN LA IDE
+
+
         RecyclerViewFragment recyclerViewFragment = new RecyclerViewFragment();
         pegarFragment(recyclerViewFragment);
 
 
+    }
+
+    private void findViewById() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
     }
 
     private void pegarFragment(RecyclerViewFragment recyclerViewFragment) {
